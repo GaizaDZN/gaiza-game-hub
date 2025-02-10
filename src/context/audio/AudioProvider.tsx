49d0881ed31e3ec8c7d76d9e267710f0604581a8
gaizadesign <brynrace@gmail.com>
2025-02-09 23:55:43 -0600

    Add and implement Howler.JS for audio context.

--- src/context/audio/AudioProvider.tsx
+++ src/context/audio/AudioProvider.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useMemo } from "react";
+import { Howl, Howler } from "howler";
 
 import { soundFiles } from "../../assets/assets";
 import { AudioContext } from "./AudioContext";
@@ -8,23 +9,51 @@ interface AudioProviderProps {
 }
 
 export function AudioProvider({ children }: AudioProviderProps) {
-  // Sounds
+  const howlInstances = useMemo<Record<string, Howl>>(() => ({}), []);
 
-  const playSound = useCallback((filename: string) => {
-    if (soundFiles[filename]) {
-      const audio = new Audio(soundFiles[filename]);
-      audio.play();
-    } else {
-      console.warn(`Sound file ${filename} not found`);
-    }
+  const playSound = useCallback(
+    (filename: string) => {
+      if (!soundFiles[filename]) {
+        console.warn(`Sound file ${filename} not found`);
+        return;
+      }
+
+      // Create a new Howl instance if it doesn't exist
+      if (!howlInstances[filename]) {
+        howlInstances[filename] = new Howl({
+          src: [soundFiles[filename]],
+          preload: true, // Preload the audio file
+          autoplay: false, // Don't autoplay
+        });
+      }
+
+      // Play the sound
+      howlInstances[filename].play();
+    },
+    [howlInstances]
+  );
+
+  const stopSound = useCallback(
+    (filename: string) => {
+      if (howlInstances[filename]) {
+        howlInstances[filename].stop();
+      }
+    },
+    [howlInstances]
+  );
+
+  const setVolume = useCallback((volume: number) => {
+    Howler.volume(volume);
   }, []);
 
   // Memoize context value to prevent unnecessary re-renders
   const value = useMemo(
     () => ({
       playSound,
+      stopSound,
+      setVolume,
     }),
-    [playSound]
+    [playSound, setVolume, stopSound]
   );
 
   return (
